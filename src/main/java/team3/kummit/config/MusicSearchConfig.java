86f28153bf028054d941dc1aa61d510ec5f49aef
jay <rlaakswo0687@gmail.com>
2025-06-30 15:48:24 +0900

    음악검색 HTTP Connection pool 적용

--- src/main/java/team3/kummit/config/MusicSearchConfig.java
+++ src/main/java/team3/kummit/config/MusicSearchConfig.java
@@ -1,8 +1,12 @@
 package team3.kummit.config;
 
+import org.apache.hc.client5.http.impl.DefaultConnectionKeepAliveStrategy;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,7 +15,20 @@ public class MusicSearchConfig {
     @Bean
     public RestTemplate getResetTemplate() {
 
-        return new RestTemplate(new SimpleClientHttpRequestFactory());
+        // 커넥션 풀 설정
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(50);
+        connectionManager.setDefaultMaxPerRoute(50); // 아이튠즈 API 만 사용하니까 개수 동일하게
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        factory.setConnectTimeout(10_000);
+
+        return new RestTemplate(factory);
     }
 
 }
